c0a20e5f3d38484326a7e5c04c309ebe707aada1
Seebii11 <36041410+1Sebi@users.noreply.github.com>
2022-11-28 21:49:13 +0100

    Add style page

--- controllers/generate-controller.js
+++ controllers/generate-controller.js
@@ -5,10 +5,20 @@ var spotifyApi = new SpotifyWebApi();
 const generateView = (req, res) => {
     spotifyApi.setAccessToken(req.query.access_token);
     
+    // Get 6m top artists
     spotifyApi.getMyTopArtists()
   .then(function(data) {
     let topArtists = data.body.items;
     console.log(topArtists);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
+  // Get 6m top tracks
+  spotifyApi.getMyTopTracks()
+  .then(function(data) {
+    let topTracks = data.body.items;
+    console.log(topTracks);
   }, function(err) {
     console.log('Something went wrong!', err);
   });
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use('/', require('./routes/router'));
+app.use(express.static('public'));
+
+app.use(function(req,res){
+    res.status(404).render('404.ejs');
+});
 
 app.listen(port, () => {
     console.log(`En ligne sur le port ${port}`)
