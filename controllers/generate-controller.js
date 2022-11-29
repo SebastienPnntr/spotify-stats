e6a2e0b1c30774a489a33ccd8c64c0b25e4793d9
1Sebi <sebastien.pnntr@eduge.ch>
2022-11-29 09:45:30 +0100

    Error handle when didn't agree to log in with Spotify

--- controllers/generate-controller.js
+++ controllers/generate-controller.js
@@ -3,31 +3,36 @@ var SpotifyWebApi = require('spotify-web-api-node');
 var spotifyApi = new SpotifyWebApi();
 
 const generateView = (req, res) => {
-    spotifyApi.setAccessToken(req.query.access_token);
-    
-    // Get 6m top artists
-    spotifyApi.getMyTopArtists()
-  .then(function(data) {
-    let topArtists = data.body.items;
-    console.log(topArtists);
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  });
+  if (req.query.access_token == "undefined") {
+    res.redirect('home');
+    throw 'You didn\'t accept the spotify accesss.';
+  }
+
+  spotifyApi.setAccessToken(req.query.access_token);
+
+  // Get 6m top artists
+  /*spotifyApi.getMyTopArtists()
+    .then(function (data) {
+      let topArtists = data.body.items;
+      console.log(topArtists);
+    }, function (err) {
+      console.log('Something went wrong!', err);
+    });
 
   // Get 6m top tracks
   spotifyApi.getMyTopTracks()
-  .then(function(data) {
-    let topTracks = data.body.items;
-    console.log(topTracks);
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  });
+    .then(function (data) {
+      let topTracks = data.body.items;
+      console.log(topTracks);
+    }, function (err) {
+      console.log('Something went wrong!', err);
+    });*/
 
-    res.render('generate', {
+  res.render('generate', {
 
-    })
+  })
 }
 
 module.exports = {
-    generateView
+  generateView
 };
